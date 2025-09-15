ca5fb9df0afd2a9b705b59c9489c4067245438e0
Oumar Diane <oumardiane399@gmail.com>
2025-09-15 15:58:09 +0200

    vv2

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174"
+      "http://mekody.com"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
